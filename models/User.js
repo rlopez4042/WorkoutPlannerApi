a496795e309a78e52761723a62eb66e3725d4aba
CoreyVee <corey.villanueva@gmail.com>
2022-03-07 15:42:17 -0600

    added user, workout, and exercises schemas

--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
 //User schema code goes here
+
+const mongoose = require('./../connection')
+const Schema =  mongoose.Schema
+
+const userSchema = new Schema({
+    username: String,
+    firstName: String,
+    lastName: String,
+    age: Number,
+    weight: Number,
+    myWorkouts: [{type: Schema.Types.ObjectId, ref:'Workout'}]
+})
+
+module.exports = mongoose.model('User',userSchema)
--- models/Workout.js
+++ models/Workout.js
@@ -1,1 +1,12 @@
 //Workout schema code goes here
+
+const mongoose = require('./../connection')
+const Schema =  mongoose.Schema
+
+const workoutSchema = new Schema({
+    name: String,
+    exercises: [{type: Schema.Types.ObjectId, ref:'Exercise'}]
+
+})
+
+module.exports = mongoose.model('Workout', workoutSchema)
