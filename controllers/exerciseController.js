4ce424c0727b1b64406b863a944a6030a7b9cbbe
Garrett <garrettpyke@msn.com>
2022-03-07 15:29:38 -0700

    added all Exercises route

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -1,1 +1,13 @@
-// exercise controller
+const express = require('express')
+const router = express.Router()
+
+const Exercise = require('./../models/Exercise')
+
+router.get("/", async (req, res) => {
+    console.log("all exercises route")
+    const allExercises = await Exercise.find({})
+    res.json({
+        status: 200,
+        Exercises: allExercises
+    })
+})
