342d33eacea15b9676dc502e82525cfd882be63e
Robert Lopez <robertlopez@Roberts-MBP.lan>
2022-03-08 10:03:09 -0800

    added crud elements

--- controllers/WorkoutController.js
+++ controllers/WorkoutController.js
@@ -1,26 +1,61 @@
-const express = require('express')
-const router = express.Router()
-
-const Exercise = require('../models/WorkoutSchema')
+const express = require("express");
+const res = require("express/lib/response");
+const router = express.Router();
+const Workout = require("../models/WorkoutSchema");
 
 router.get("/", async (req, res) => {
-    console.log("all workouts route")
-    const allWorkouts = await Exercise.find({})
+  console.log("all workouts route");
+  const allWorkouts = await Workout.find({});
+  res.json({
+    status: 200,
+    Workout: allWorkouts,
+  });
+});
+
+router.get("/:name", (req, res) => {
+  Workout.findOne({ name: req.params.name }).then((workout) => {
     res.json({
-        status: 200,
-        Exercises: allWorkouts
-    })
-})
+      status: 200,
+      workout: workout,
+    });
+  });
+});
 
 router.post("/", async (req, res) => {
-    const data = req.body;
-    const newExercise = await Exercise.create(data)
-    res.json({
-        status: 200,
-        msg: 'New Exercises added',
-        Excercise: newExercise
+  const data = req.body;
+  const newWorkout = await Workout.create(data);
+  res.json({
+    status: 200,
+    msg: "New Exercises added",
+    Workout: newWorkout,
+  });
+});
 
+router.put("/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      duration: req.body.duration,
+      exercises: req.body.exercises,
+    },
+    { new: true }
+  ).then((workout) =>
+    res.json({
+      status: 200,
+      msg: "workout updated",
+      workout: workout,
+    })
+  );
+});
+
+router.delete("/:id", (req, res) => {
+  Workout.findByIdAndDelete(req.params.id).then((workout) => {
+    res.json({
+      status: 200,
+      msg: workout,
     });
+  });
 });
 
 module.exports = router;
